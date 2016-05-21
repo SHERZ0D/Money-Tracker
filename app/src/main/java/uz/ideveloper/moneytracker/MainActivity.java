5b57dbf53404d805e38f670dd201651f3c535dc5
shuxratov Sherzod <sherwww@mail.ru>
2016-05-21 14:20:03 +0500

    added  list_view, transactionAdapter.

--- app/src/main/java/uz/ideveloper/moneytracker/MainActivity.java
+++ app/src/main/java/uz/ideveloper/moneytracker/MainActivity.java
@@ -1,13 +1,34 @@
 package uz.ideveloper.moneytracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private TransactionAdapter transactionAdapter;
+    List<Transaction> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Transaction> adapterData = getDataList();
+        transactionAdapter = new TransactionAdapter(this, adapterData);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(transactionAdapter);
+    }
+
+    private List<Transaction> getDataList() {
+        data.add(new Transaction("TelePhone", "2000"));
+        data.add(new Transaction("T-Shorts", "300"));
+        data.add(new Transaction("Jeans", "2000"));
+        return data;
     }
 }
+
